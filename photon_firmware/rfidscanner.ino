89419ad07cdf5730aacdb3297112996a9154ffab
Austin Dudas <dudasaus@umich.edu>
2018-04-06 14:11:07 -0400

    fixed spacing in firmware code

--- photon_firmware/rfidscanner.ino
+++ photon_firmware/rfidscanner.ino
@@ -4,7 +4,7 @@
 #define RST_PIN D2
 
 // Create MFRC522 instance.
-MFRC522 mfrc522(SS_PIN, RST_PIN);	
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 // Big endian getID
 unsigned int getID(byte* arr) {
@@ -12,33 +12,32 @@ unsigned int getID(byte* arr) {
 }
 
 void setup() {
-	Serial.begin(9600);	
+	Serial.begin(9600);
 	mfrc522.setSPIConfig();
-
 	mfrc522.PCD_Init();
 }
 
 void loop() {
-    
+
 	// Look for new cards
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
+	if (!mfrc522.PICC_IsNewCardPresent()) {
 		return;
 	}
 
 	// Select one of the cards
-	if ( ! mfrc522.PICC_ReadCardSerial()) {
+	if (!mfrc522.PICC_ReadCardSerial()) {
 		return;
 	}
 
 	// Get id in hex format
 	unsigned int id = getID(mfrc522.uid.uidByte);
-    char str[16];
-    sprintf(str, "%02x", id);
-    
-    // Publish scan event with id
-    Serial.println(str);
-    Particle.publish("rfid-scan", str);
-    
-    // Don't repeat the same card
-    mfrc522.PICC_HaltA();
+  char str[16];
+  sprintf(str, "%02x", id);
+
+  // Publish scan event with id
+  Serial.println(str);
+  Particle.publish("rfid-scan", str);
+
+  // Don't repeat the same card
+  mfrc522.PICC_HaltA();
 }
